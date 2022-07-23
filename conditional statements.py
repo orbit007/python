f1ab6b5a5b5e4d26f970cdc434bbd44fa7d2eb8e
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-07-22 21:40:02 -0700

    Update conditional statements.py

--- conditional statements.py
+++ conditional statements.py
@@ -17,3 +17,21 @@ elif num>=40:
     print("3rd")
 else:
     print("fail")
+
+
+
+
+# simple calculator
+
+n1=int(input("enter number1"))
+n2=int(input("enter number2"))
+x=int(input("enter a numner for 1 add , 2 sub ,3 mul , 4 div"))
+
+if x==1:
+    print(n1+n2)
+elif x==2:
+    print(n1-n2)
+elif x==3:
+    print(n1*n2)
+else:
+    print(n1/n2)
