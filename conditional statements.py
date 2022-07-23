9a85c7d75c590cd1d5b2ea7680d5cda45bc2dda9
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-07-22 20:52:24 -0700

    Create conditional statements.py

---
+++ conditional statements.py
@@ -0,0 +1,19 @@
+a=int(input("enter a number"))
+
+if a%2==0:
+    print(a,"even number");
+
+else:
+    print(a,"odd number");
+
+
+num=int(input("enter your number"))
+
+if num>=60:
+    print("1st")
+elif num>=50:
+    print("2nd")
+elif num>=40:
+    print("3rd")
+else:
+    print("fail")
