7f712df55e9c81fbc6984a5d577ecdfc1341e001
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-07-22 20:16:26 -0700

    Create userInputandTypecasd.py

---
+++ userInputandTypecasd.py
@@ -0,0 +1,18 @@
+a=input("enter a number1")
+b=input("enter a number2")
+
+print(a+b)
+
+
+
+x=int(input("enter number1"))
+y=int(input("enter number2"))
+
+print(x+y)
+
+
+
+m=eval(input("enter number1"))
+n=eval(input("enter number2"))
+
+print(m+n)
