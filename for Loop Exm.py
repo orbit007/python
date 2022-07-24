9de16b095d1d6d6f6bcf2f2e943130cadd113b98
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-07-23 19:17:41 -0700

    Create for Loop Exm.py

---
+++ for Loop Exm.py
@@ -0,0 +1,27 @@
+for n in range(5):
+    print(n)
+
+
+
+for n in range(1,10,1):
+    print(n)
+
+
+print()
+
+for n in range(1,11):
+    a=2
+    print(a,"*",n,"=",a*n)
+
+
+print()
+
+for n in range(10,0,-1):
+    print(n)
+
+
+
+print()
+
+for n in range(10,-1,-2):
+    print(n)
