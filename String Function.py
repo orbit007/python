295bf402eb7d941c4b527be255494fbbe8f42ac0
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-07-24 13:34:53 -0700

    Update String Function.py

--- String Function.py
+++ String Function.py
@@ -10,3 +10,27 @@ print(t)
 
 c=n.capitalize()
 print(c)
+
+
+
+# find() index()  isalpha()  isdigit()  isalnum()
+
+print(w.find('E'))
+
+print(w.find('E',7))
+
+print(w.find('Z'))
+
+#index
+
+print(w.index('E'))
+print(w.index('E',7))
+
+#isalpha()  isdigit()  isalnum()
+
+print(w.isalpha())
+
+print(w.isdigit())
+
+print(w.isalnum())
+
