de288d5a17f016d5acc323fec65626cae89ecf54
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-07-23 20:52:00 -0700

    Create string Indx Slicing Iteration.py

---
+++ string Indx Slicing Iteration.py
@@ -0,0 +1,16 @@
+w="abhishek kumar"
+print(w[1])
+print(w[0:8])
+print(w[0::2])
+print(w[-1::-1])
+
+for a in range(len(w)):
+    print(w[a])
+
+for n in range(len(w)-1,-1,-1):
+    print(w[n])
+
+print()
+
+for x in w:
+    print(x)
