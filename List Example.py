252a578a01e11c0bf8d77c73af98c3a9762ca258
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-07-24 20:38:31 -0700

    Update List Example.py

--- List Example.py
+++ List Example.py
@@ -34,3 +34,43 @@ print()
 
 for n in range(t-1,-1,-1):
     print(l[n])
+
+
+
+for n in range(t-1,-1,-1):
+    print(l[n])
+
+# list comprehension
+
+k=[]
+for n in range(20):
+    k.append(n)
+print(k)
+
+m=[]
+for i in range(6):
+    j=int(input("enter"))
+    m.append(j)
+   # m.append(int(input("enter")))
+
+print(m)
+
+h=[]
+
+
+for i in range(6):
+    k=int(input("enter"))
+    if k%2==0:
+        h.append(k)
+print(h)
+
+s="abhishek"
+n=[]
+for j in range(len(s)):
+    n.append(s[j])
+print(n)
+
+o=[]
+for l in s:
+    o.append(l)
+print(o)
