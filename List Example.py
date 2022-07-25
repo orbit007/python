5d8152f3069706bfdfbd25aae272e821bfe53f4d
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-07-24 23:16:26 -0700

    Update List Example.py

--- List Example.py
+++ List Example.py
@@ -74,3 +74,52 @@ o=[]
 for l in s:
     o.append(l)
 print(o)
+
+
+#list function------------------------------------------------------------------
+
+
+l=[20,30,40,50,60,70]
+print(l)
+
+del l[1]
+print(l)
+
+l.pop(2)
+print(l)
+print(l.pop(2))
+print(l)
+
+l.remove(70)
+print(l)
+l.clear()
+print(l)
+
+k=[1,2,3,4,5,6,7,8]
+k[0]=10
+print(k)
+
+#insert()------>kishi v jghe pr insert kr skte hai (take two args)
+#append()------>last ke side me add hoga---->jesa data type wese utha kr aandar rkh dega
+#extends()----->ye andar le value pr kaaam krta hai
+
+m=[20,30,40,50]
+
+m.insert(0,80)
+print(m)
+
+n=[1,2]
+m.append(n)
+m.append(105)
+print(m)
+
+m.extend(n)
+print(m)
+
+m.insert(1,200)
+print(m)
+
+m.append(1000)
+print(m)
+
+
