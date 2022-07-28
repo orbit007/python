190a19d6beedbfdecb4fb542fb9f15f4bc4418b8
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-07-28 16:03:24 -0700

    Create TupleInPython.py

---
+++ TupleInPython.py
@@ -0,0 +1,58 @@
+t=(10,20,30,40,40,10,50,10,80,30,20)
+print(t)
+a=t[1]
+print(a)
+
+
+
+for i in range(len(t)):
+    print(t[i])
+
+
+print()
+
+
+for n in t:
+    print(n)
+
+
+
+#min()
+#max()
+#index()
+#count()
+#sum()
+
+
+
+print()
+b=min(t)
+print(b)
+
+
+
+c=max(t)
+print(c)
+
+
+
+x=t.count(10)
+print(x)
+
+
+q=t.index(80)
+print(q)
+
+
+print(t[8])
+
+
+w=sum(t)
+print(w)
+
+t2=(1,2,3,4,11,22,)
+
+
+for i in t2:
+    print(i)
+    
