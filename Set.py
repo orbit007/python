fd955f8e7812a570d34fe2c35dc94763a4696f8c
orbit007 <68804524+orbit007@users.noreply.github.com>
2022-07-30 00:18:26 -0700

    Create Set.py

---
+++ Set.py
@@ -0,0 +1,35 @@
+#unorder
+#unindex
+#set()|{}
+
+s={10,20,30,40}
+print(s)
+for a in s:
+    print(a)
+#set()
+#add()
+#pop()
+#renove()
+#clear()
+#discard()
+#update()]
+
+l=[40,50,60,70]
+
+k=set(l)
+print(k)
+
+s.add(100)
+print(s)
+
+s.pop()
+print(s)
+
+s.remove(30)
+print(s)
+
+s.discard(10)
+print(s)
+m=[40,30,606,70]
+s.update(m)
+print(s)
